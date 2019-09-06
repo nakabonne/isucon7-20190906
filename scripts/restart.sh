b3d131968a10ab8e6cc1d54829e5919a00bf5ab1
Ryo Nakao <nakabonne@gmail.com>
2019-09-06 13:53:33 +0900

    Update restart script

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,35 +2,34 @@
 set -e
 
 # pull
-cd /var/www/html/webapp
 git pull
 echo "$(tput setaf 2)Pull Succeeded! $(tput sgr0)"
 echo ""
 
 # reverse proxy reload
-sudo cp etc/nginx.conf /etc/nginx/nginx.conf
+sudo cp filis/app/nginx.conf /etc/nginx/nginx.conf
 sudo /usr/sbin/nginx -t
 sudo service nginx reload
 echo "$(tput setaf 2)reverse proxy reload Succeeded! $(tput sgr0)"
 echo ""
 
 # in-memory cache reload
-sudo cp etc/redis.conf /etc/redis/redis.conf
-sudo systemctl restart redis-server
-echo "$(tput setaf 2)in-memory cache reload Succeeded! $(tput sgr0)"
-echo ""
+#sudo cp etc/redis.conf /etc/redis/redis.conf
+#sudo systemctl restart redis-server
+#echo "$(tput setaf 2)in-memory cache reload Succeeded! $(tput sgr0)"
+#echo ""
 
 # log rotate
-make rotate
-make mysqldumpslow
-echo "$(tput setaf 2)log rotate Succeeded! $(tput sgr0)"
-echo ""
+#make rotate
+#make mysqldumpslow
+#echo "$(tput setaf 2)log rotate Succeeded! $(tput sgr0)"
+#echo ""
 
 # application reload
-sudo cp etc/systemd.go.service /etc/systemd/system/systemd.go.service
-sudo systemctl daemon-reload
-cd go
-make build
+#sudo cp etc/systemd.go.service /etc/systemd/system/systemd.go.service
+#sudo systemctl daemon-reload
+cd webapp/go/
+make
 sudo systemctl restart systemd.go.service
 echo "$(tput setaf 2)application reload Succeeded! $(tput sgr0)"
 echo ""
